7f9c1491390d4f09694bf8609565372dbff624ed
Lukas Carton <carton.lukas@gmail.com>
2020-08-25 15:58:07 +0200

    first commit

---
+++ IntervalRunning/IntervalRunning/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IntervalRunning.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command StartRunningCommand { get; }
+
+        public MainPageViewModel()
+        {
+            StartRunningCommand = new Command(() =>
+            {
+                Console.WriteLine("----------- Start tracking... ------------");
+            });
+        }
+    }
+}
